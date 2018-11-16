c91ece16d64b52476b0d019d9403e241612394b9
Jay1305 <nowhere>
2018-11-15 21:34:29 -0800

    Created Identity Server project and added config class for GetAllAPIResources and GetClient

---
+++ BankOfDotNet.IdentitySvr/Config.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace BankOfDotNet.IdentitySvr
+{
+    public class Config
+    {
+
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource> { new ApiResource("BankOfDotNetAPI","Customer API for BankOfDotNet")};
+        }
+
+        public static IEnumerable<Client> GetClient()
+        {
+            return new List<Client> {
+                new Client {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = {new Secret("secret".Sha256()) },
+                    AllowedScopes={"bankOfDotNetApi" }
+                } };
+        }
+    }
+}
